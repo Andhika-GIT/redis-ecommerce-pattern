f127a930bc65364cdb8eb3f70785dabd46ad60f8
Andhika Prasetya <andhikapr081@gmail.com>
2023-12-26 11:43:55 +0700

    feat : serialize user before saving to redis

--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -2,7 +2,15 @@ import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
 import { client } from '$services/redis';
 import { usersKey } from '$services/keys';
+import type { Attribute } from 'svelte/types/compiler/interfaces';
 
+
+const serialize = (user : CreateUserAttrs) => {
+    return {
+        username : user.username,
+        password : user.password
+    }
+}
 
 export const getUserByUsername = async (username: string) => {};
 
@@ -12,11 +20,11 @@ export const createUser = async (attrs: CreateUserAttrs) => {
     const id = genId()
     await client.HSET(
         usersKey(id),
-        {
-            username : attrs.username,
-            password : attrs.password
-        }
+        serialize(attrs)
     )
 
     return id
 };
+
+
+ 
