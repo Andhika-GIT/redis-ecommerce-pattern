1c71976f2983f946e6c82372149e3c6955202497
Andhika Prasetya <andhikapr081@gmail.com>
2023-12-26 11:13:14 +0700

    feat: redis-save user implmenetation

--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -1,8 +1,22 @@
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
+import { client } from '$services/redis';
+import { usersKey } from '$services/keys';
+
 
 export const getUserByUsername = async (username: string) => {};
 
 export const getUserById = async (id: string) => {};
 
-export const createUser = async (attrs: CreateUserAttrs) => {};
+export const createUser = async (attrs: CreateUserAttrs) => {
+    const id = genId()
+    await client.HSET(
+        usersKey(id),
+        {
+            username : attrs.username,
+            password : attrs.password
+        }
+    )
+
+    return id
+};
