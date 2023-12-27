a32dc01577ff53a0b9adb8c955ff907c09a86126
Andhika-GIT <andhikapr081@gmail.com>
2023-12-27 15:46:59 +0700

    practice : how pipeline works

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -1,5 +1,26 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
 
-const run = async () => {};
+const run = async () => {
+	await client.HSET('car1', {
+		color: 'red',
+		year: 1950
+	});
+	await client.HSET('car2', {
+		color: 'blue',
+		year: 1951
+	});
+	await client.HSET('car3', {
+		color: 'green',
+		year: 1952
+	});
+
+	const results = await Promise.all([
+		client.HGETALL('car1'),
+		client.HGETALL('car2'),
+		client.HGETALL('car3')
+	]);
+
+	console.log(results);
+};
 run();
