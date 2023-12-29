5404d69fbed2ea3cbba66935b42e0856a5fa9389
Andhika Prasetya <andhikapr081@gmail.com>
2023-12-29 11:54:50 +0700

    feat : like count using SET and H increase

--- src/services/queries/likes.ts
+++ src/services/queries/likes.ts
@@ -1,21 +1,42 @@
 import { client } from "$services/redis";
 
-import { userLikesKey } from "$services/keys";
+import { itemKey, userLikesKey } from "$services/keys";
 
-export const likedItems = async (userId: string) => {};
-
-export const likeItem = async (itemId: string, userId: string) => {
-    await client.SADD(
+export const userLikesItem = async (itemId: string, userId: string) => {
+    const alreadyLikePost = await client.SISMEMBER(
         userLikesKey(userId),
         itemId
     )
+
+    if (alreadyLikePost) return true
+
+    return false
+}
+
+export const likedItems = async (userId: string) => {
+   
+};
+
+export const likeItem = async (itemId: string, userId: string) => {
+   const inserted =  await client.SADD(
+        userLikesKey(userId),
+        itemId
+    )
+
+    if (inserted) {
+        return client.HINCRBY(itemKey(itemId), 'likes', 1)
+    }
 };
 
 export const unlikeItem = async (itemId: string, userId: string) => {
-    await client.SREM(
+   const removed =  await client.SREM(
         userLikesKey(userId),
         itemId
     )
+
+    if (removed) {
+        return client.HINCRBY(itemKey(itemId), 'likes', -1)
+    }
 };
 
 export const commonLikedItems = async (userOneId: string, userTwoId: string) => {};
