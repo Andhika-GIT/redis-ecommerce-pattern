c8ff20079e8f1b9f9879e0d61cf41bcc17ebddaf
Andhika Prasetya <andhikapr081@gmail.com>
2023-12-30 14:17:59 +0700

    feat : fetch all items based on ids and return to client

--- src/services/queries/items/by-ending-time.ts
+++ src/services/queries/items/by-ending-time.ts
@@ -1,5 +1,6 @@
-import { itemByEndingAtKey } from "$services/keys";
+import { itemByEndingAtKey, itemKey } from "$services/keys";
 import { client } from "$services/redis";
+import { deserialize } from "./deserialize";
 
 
 export const itemsByEndingTime = async (
@@ -7,7 +8,10 @@ export const itemsByEndingTime = async (
 	offset = 0,
 	count = 10
 ) => {
-	const ids = await client.ZRANGE(itemByEndingAtKey(), 130, '+inf', {
+
+	// sorted the item based item that ending soon
+	// this will return item id based on item that ending soon
+	const ids = await client.ZRANGE(itemByEndingAtKey(), Date.now(), '+inf', {
 		BY: 'SCORE',
 		LIMIT: {
 			offset: offset,
@@ -16,5 +20,13 @@ export const itemsByEndingTime = async (
 	}
 	)
 
-	console.log(ids)
+	// pipeline to get all items based on the ids
+	const results = await Promise.all(
+		ids.map(id => client.HGETALL(itemKey(id)))
+	)
+
+	const items = results.map((item, i) => deserialize(ids[i],item))
+
+	return items
+	
 };
