123e2f4aecefbf4e0d6ac93f8d1a3dda9d0da5a4
Andhika Prasetya <andhikapr081@gmail.com>
2023-12-30 10:40:42 +0700

    feat : increment the views property for hash & sorted set

--- src/services/queries/views.ts
+++ src/services/queries/views.ts
@@ -1,1 +1,26 @@
-export const incrementView = async (itemId: string, userId: string) => {};
+import { client } from "$services/redis";
+
+import { itemKey, itemByViewsKey } from "$services/keys";
+
+export const incrementView = async (itemId: string, userId: string) => {
+
+    await Promise.all([
+        // increment the item for 'views' property for hash data
+        client.HINCRBY(
+            itemKey(itemId),
+            'views',
+            1
+        ),
+
+        // increment the item 'views' score for sorted set
+        client.ZINCRBY(
+            itemByViewsKey(),
+            1,
+            itemId
+        )
+    ])
+  
+
+
+    
+};
