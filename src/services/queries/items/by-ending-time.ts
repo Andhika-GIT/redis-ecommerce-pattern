b3f8a012bdd1f525abc6b22b93e9143b8d907d2d
Andhika Prasetya <andhikapr081@gmail.com>
2023-12-30 14:03:22 +0700

    feat : query to take item that ending soon using ZRANGE

--- src/services/queries/items/by-ending-time.ts
+++ src/services/queries/items/by-ending-time.ts
@@ -1,5 +1,20 @@
+import { itemByEndingAtKey } from "$services/keys";
+import { client } from "$services/redis";
+
+
 export const itemsByEndingTime = async (
 	order: 'DESC' | 'ASC' = 'DESC',
 	offset = 0,
 	count = 10
-) => {};
+) => {
+	const ids = await client.ZRANGE(itemByEndingAtKey(), 130, '+inf', {
+		BY: 'SCORE',
+		LIMIT: {
+			offset: offset,
+			count: count
+		}
+	}
+	)
+
+	console.log(ids)
+};
