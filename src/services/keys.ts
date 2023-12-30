6615e269978b4cad853f49c65b2f118f1a8b5f25
Andhika Prasetya <andhikapr081@gmail.com>
2023-12-30 10:27:18 +0700

    feat : items by views logic using sorted set create value of id and score of 0 when creating new items later on, we can sorted the items by views, using the highest score

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -1,7 +1,12 @@
 export const pageCacheKey = (id: string) => `pagecache#${id}`;
 export const usersKey = (userId: string) => `users#${userId}`;
 export const sessionKey = (sessionId: string) => `session#${sessionId}`;
-export const itemKey = (itemId: string) => `item#${itemId}`;
+
 export const usernameUniquekey = () => 'usernames:unique'
 export const userLikesKey = (userId: string) => `users:likes#${userId}`
 export const usernamesKey = () => 'usernames'
+
+
+// items
+export const itemKey = (itemId: string) => `item#${itemId}`;
+export const itemByViewsKey = () => 'items:views'
--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -2,7 +2,7 @@ import type { CreateItemAttrs } from '$services/types';
 import { client } from '$services/redis';
 import { serialize } from './serialize';
 import { genId } from '$services/utils';
-import { itemKey } from '$services/keys';
+import { itemKey, itemByViewsKey } from '$services/keys';
 import { deserialize } from './deserialize';
 
 export const getItem = async (id: string) => {
@@ -36,7 +36,26 @@ export const getItems = async (ids: string[]) => {
 
 export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
 	const id = genId();
-	await client.HSET(itemKey(id), serialize(attrs));
+
+	// PIPELINE to run all query in single request
+	await Promise.all([
+
+		// create new keys and score using sorted sets ( to display most view item in dashboard )
+		client.ZADD(
+			itemByViewsKey(),
+			{
+				// insert the item id as member, and views total as score 
+				// the default views will 0, because it's a new item
+				value: id,
+				score: 0,
+				
+			}
+		),
+
+
+		// create new item with id and it's property using hash
+		client.HSET(itemKey(id), serialize(attrs))
+	])
 
 	return id;
 };
