9b16e04a5104387751b854c8948c8332ee2027a9
Andhika-GIT <andhikapr081@gmail.com>
2023-12-31 15:06:04 +0700

    feat : items by views, specify order and limit using SORT command

--- src/services/queries/items/by-views.ts
+++ src/services/queries/items/by-views.ts
@@ -1,8 +1,8 @@
-import { client } from "$services/redis";
-import { itemKey, itemByViewsKey } from "$services/keys";
+import { client } from '$services/redis';
+import { itemKey, itemByViewsKey } from '$services/keys';
 
 export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {
-    /*
+	/*
 
     1. GET # ->get all the member from items:views sorted set ( which already sorted by score ), and put it as the refrence for the backtick ( * ) in the next process.
 
@@ -12,17 +12,18 @@ export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, c
     3.  `${itemKey('*')}->views` -> based on the previous process, the backtick ( * ) represents all the members from items:views sorted set, and since the members are the item id, so we point out the itemKey hash data based on the backtick (*) which represent the id, and get the field 'views'
       ex : itemKey('testid') then get get item:testid, then get the 'views' field value
      */
-    const results = await client.SORT(itemByViewsKey(),{
-        GET: [
-            '#',
-            `${itemKey('*')}->name`,
-            `${itemKey('*')}->views`,
-        ],
-        // nosort -> get the result of the default sorting that done by items:views sorted set, which sort the items:views by score
-        BY : 'nosort'
-    })
+	const results = await client.SORT(itemByViewsKey(), {
+		GET: ['#', `${itemKey('*')}->name`, `${itemKey('*')}->views`],
+		// nosort -> get the result of the default sorting that done by items:views sorted set, which sort the items:views by score
+		BY: 'nosort',
+		// specify the DIRECTION, either 'ASC' or 'DSC' (we got this value as argument in this function)
+		DIRECTION: order,
+		// spesify start index and count (total item) that shows in the client
+		LIMIT: {
+			offset: offset,
+			count: count
+		}
+	});
 
-   
-
-    console.log(results)
+	console.log(results);
 };
