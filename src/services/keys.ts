fe98ec8bb2be2415d4915f347813931daaf2a097
Andhika-GIT <andhikapr081@gmail.com>
2024-01-02 22:19:11 +0700

    feat : using list to add and retrieve item bids

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -2,13 +2,13 @@ export const pageCacheKey = (id: string) => `pagecache#${id}`;
 export const usersKey = (userId: string) => `users#${userId}`;
 export const sessionKey = (sessionId: string) => `session#${sessionId}`;
 
-export const usernameUniquekey = () => 'usernames:unique'
-export const userLikesKey = (userId: string) => `users:likes#${userId}`
-export const usernamesKey = () => 'usernames'
-
+export const usernameUniquekey = () => 'usernames:unique';
+export const userLikesKey = (userId: string) => `users:likes#${userId}`;
+export const usernamesKey = () => 'usernames';
 
 // items
 export const itemKey = (itemId: string) => `item#${itemId}`;
-export const itemByViewsKey = () => 'items:views'
-export const itemByEndingAtKey = () => 'items:endingAt'
-export const itemUniqueViewsKey = (userId: string) => `items:views#${userId}`
+export const itemByViewsKey = () => 'items:views';
+export const itemByEndingAtKey = () => 'items:endingAt';
+export const itemUniqueViewsKey = (userId: string) => `items:views#${userId}`;
+export const itemBidHistoryKey = (itemId: string) => `history#${itemId}`;
--- src/services/queries/bids.ts
+++ src/services/queries/bids.ts
@@ -1,6 +1,29 @@
 import type { CreateBidAttrs, Bid } from '$services/types';
+import { itemBidHistoryKey } from '$services/keys';
+import { client } from '$services/redis';
+import { DateTime } from 'luxon';
 
-export const createBid = async (attrs: CreateBidAttrs) => {};
+const serializeHistory = (amount: number, createdAt: number) => {
+	return `${amount}:${createdAt}`;
+};
+
+const deserializeHistory = (stored: string) => {
+	const [amount, createdAt] = stored.split(':');
+
+	return {
+		amount: parseFloat(amount),
+		createdAt: DateTime.fromMillis(parseInt(createdAt))
+	};
+};
+
+export const createBid = async (attrs: CreateBidAttrs) => {
+	const convertedDate = attrs.createdAt.toMillis();
+
+	await client.RPUSH(
+		itemBidHistoryKey(attrs.itemId),
+		serializeHistory(attrs.amount, convertedDate)
+	);
+};
 
 export const getBidHistory = async (itemId: string, offset = 0, count = 10): Promise<Bid[]> => {
 	return [];
