57e1377794a9e2341731bf51d6e1a34881be7653
Andhika-GIT <andhikapr081@gmail.com>
2024-01-05 21:21:15 +0700

    feat : add bids logic and display item by price using SORT

--- src/services/queries/bids.ts
+++ src/services/queries/bids.ts
@@ -20,64 +20,49 @@ const deserializeHistory = (stored: string) => {
 export const createBid = async (attrs: CreateBidAttrs) => {
 	// create isolated connection for this particular action
 	return client.executeIsolated(async (isolatedClient) => {
+		// watch for this spesific key item
+		await isolatedClient.WATCH(itemKey(attrs.itemId));
+		//  if it change, cancel all action and the transaction below (to avoid double same value)
 
+		// validation 1 : check if item exist
+		const item = await getItem(attrs.itemId);
 
-	// watch for this spesific key item
-	await isolatedClient.WATCH(itemKey(attrs.itemId))
-	//  if it change, cancel all action and the transaction below (to avoid double same value)
-		
-	// validation 1 : check if item exist
-	const item = await getItem(attrs.itemId)
-
-	if (!item) {
-		throw new Error("item doesn't exist")
-	}
-	// validation 2 : check if item expired 
-	if (item.endingAt.diff(DateTime.now()).toMillis() < 0) {
-		throw new Error("item closed to binding")
-	}
-
-
-	// validation 3 : check if user input bid amount is greater than current amount
-	if (item.price >= attrs.amount){
-		throw new Error("bid is too low")
-	}
-
-
-
-	// create new list
-	const convertedDate = attrs.createdAt.toMillis();
-
-
-	// run transaction below, if the pointed item in isolatedClient.WATCH doesn't change
-	return isolatedClient
-	.multi() // begin transaction
-	// create new list for spesific item
-	.RPUSH(
-			itemBidHistoryKey(attrs.itemId),
-			serializeHistory(attrs.amount, convertedDate)
-		)
-	// update the item in the hash based on the spesific key id
-	.HSET(
-			itemKey(item.id), {
-				bids: item.bids + 1,
-				price: attrs.amount,
-				highestBidUserId: attrs.userId
-			}
-		)
-	.ZADD(
-		itemByPriceKey(),
-		{
-			value: item.id,
-			score: attrs.amount
+		if (!item) {
+			throw new Error("item doesn't exist");
 		}
-	)
+		// validation 2 : check if item expired
+		if (item.endingAt.diff(DateTime.now()).toMillis() < 0) {
+			throw new Error('item closed to binding');
+		}
 
-	})
+		// validation 3 : check if user input bid amount is greater than current amount
+		if (item.price >= attrs.amount) {
+			throw new Error('bid is too low');
+		}
+
+		// create new list
+		const convertedDate = attrs.createdAt.toMillis();
+
+		// run transaction below, if the pointed item in isolatedClient.WATCH doesn't change
+		return (
+			isolatedClient
+				.multi() // begin transaction
+				// create new list for spesific item
+				.RPUSH(itemBidHistoryKey(attrs.itemId), serializeHistory(attrs.amount, convertedDate))
+				// update the item in the hash based on the spesific key id
+				.HSET(itemKey(item.id), {
+					bids: item.bids + 1,
+					price: attrs.amount,
+					highestBidUserId: attrs.userId
+				})
+				.ZADD(itemByPriceKey(), {
+					value: item.id,
+					score: attrs.amount
+				})
+				.EXEC()
+		);
+	});
 	// update the bid score for sorted set based on spesific id member
-	
-		
-	
 };
 
 export const getBidHistory = async (itemId: string, offset = 0, count = 10): Promise<Bid[]> => {
--- src/services/queries/items/by-price.ts
+++ src/services/queries/items/by-price.ts
@@ -1,1 +1,40 @@
-export const itemsByPrice = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {};
+import { client } from '$services/redis';
+import { itemKey, itemByViewsKey, itemByPriceKey } from '$services/keys';
+import { deserialize } from './deserialize';
+
+export const itemsByPrice = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {
+	let results: any = await client.SORT(itemByPriceKey(), {
+		GET: [
+			'#',
+			`${itemKey('*')}->name`,
+			`${itemKey('*')}->views`,
+			`${itemKey('*')}->endingAt`,
+			`${itemKey('*')}->imageUrl`,
+			`${itemKey('*')}->price`
+		],
+		// nosort -> get the result of the default sorting that done by items:views sorted set, which sort the items:views by score
+		BY: 'nosort',
+		// specify the DIRECTION, either 'ASC' or 'DSC' (we got this value as argument in this function)
+		DIRECTION: order,
+		// spesify start index and count (total item) that shows in the client
+		LIMIT: {
+			offset: offset,
+			count: count
+		}
+	});
+
+	const items = [];
+
+	while (results.length) {
+		// parse the results
+		// take the id, name, views, endingAt, imageUrl, price frm results array data
+		const [id, name, views, endingAt, imageUrl, price, ...rest] = results;
+		const item = deserialize(id, { name, views, endingAt, imageUrl, price });
+
+		// push it into new items array variabel
+		items.push(item);
+		results = rest;
+	}
+
+	return items;
+};
