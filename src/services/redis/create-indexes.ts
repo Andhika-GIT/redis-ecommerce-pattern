5799fbd7e5e936cd05a47bac75db18c30a3aa9a0
Andhika Prasetya <andhikapr081@gmail.com>
2024-01-07 09:26:26 +0700

    feat : check if item key index already exist return early if item key index exist

--- src/services/redis/create-indexes.ts
+++ src/services/redis/create-indexes.ts
@@ -3,6 +3,17 @@ import { itemIndexKey, itemKey } from "$services/keys";
 import { client } from "./client";
 
 export const createIndexes = async () => {
+    // take all indexes
+    const indexes = await client.ft._LIST()
+
+    // check if item index key already exist inside index list
+    const exist = indexes.find((index) => index === itemIndexKey())
+
+
+    if (exist) {
+        return
+    }
+
     return client.ft.create(
         // create index
         itemIndexKey(),
