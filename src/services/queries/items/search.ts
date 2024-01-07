8b8d47a86ff115e822f29381d4ac3a4916a3d13c
Andhika-GIT <andhikapr081@gmail.com>
2024-01-07 16:31:34 +0700

    feat : query to prioritize search result for name instead description

--- src/services/queries/items/search.ts
+++ src/services/queries/items/search.ts
@@ -1,34 +1,32 @@
-import { client } from "$services/redis";
-import { deserialize } from "./deserialize";
-import { itemIndexKey } from "$services/keys";
+import { client } from '$services/redis';
+import { deserialize } from './deserialize';
+import { itemIndexKey } from '$services/keys';
 
 export const searchItems = async (term: string, size: number = 5) => {
-    const cleaned = term
-    .replaceAll(/[^a-zA-Z ]/g, '')
-    .trim()
-    .split(' ')
-    .map(word => word ? `%${word}%` : '')
-    .join(" ")
+	const cleaned = term
+		.replaceAll(/[^a-zA-Z ]/g, '')
+		.trim()
+		.split(' ')
+		.map((word) => (word ? `%${word}%` : ''))
+		.join(' ');
 
+	// check if cleaned is empty
+	if (cleaned === '') {
+		return [];
+	}
 
-    // check if cleaned is empty
-    if (cleaned === "") {
-        return []
-    }
+	// write a query, to prioritize search result for "name" field instead of "description" field in item hashes, by putting "weight"
+	const query = `(@name:(${cleaned}) => { $weight: 5.0 }) | (@description:(${cleaned}))`;
 
-    // run the search query
-    const results = await client.ft.SEARCH(
-        itemIndexKey(),
-        cleaned, 
-        {
-            // specify the limit 
-            LIMIT: {
-                from: 0,
-                size : 5,
-            }
-        }
-    )
+	// run the search query
+	const results = await client.ft.SEARCH(itemIndexKey(), query, {
+		// specify the limit
+		LIMIT: {
+			from: 0,
+			size: 5
+		}
+	});
 
-    // map over the results, and deserialize documents array of object property 
-    return results.documents.map((result) => deserialize(result.id, result.value as any))
+	// map over the results, and deserialize documents array of object property
+	return results.documents.map((result) => deserialize(result.id, result.value as any));
 };
