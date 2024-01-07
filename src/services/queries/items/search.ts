e902343fd441eab035d928eb0b21f3393bc812f0
Andhika Prasetya <andhikapr081@gmail.com>
2024-01-07 12:33:05 +0700

    feat : implement validation and search query, map the results

--- src/services/queries/items/search.ts
+++ src/services/queries/items/search.ts
@@ -1,3 +1,7 @@
+import { client } from "$services/redis";
+import { deserialize } from "./deserialize";
+import { itemIndexKey } from "$services/keys";
+
 export const searchItems = async (term: string, size: number = 5) => {
     const cleaned = term
     .replaceAll(/[^a-zA-Z ]/g, '')
@@ -5,4 +9,26 @@ export const searchItems = async (term: string, size: number = 5) => {
     .split(' ')
     .map(word => word ? `%${word}%` : '')
     .join(" ")
+
+
+    // check if cleaned is empty
+    if (cleaned === "") {
+        return []
+    }
+
+    // run the search query
+    const results = await client.ft.SEARCH(
+        itemIndexKey(),
+        cleaned, 
+        {
+            // specify the limit 
+            LIMIT: {
+                from: 0,
+                size : 5,
+            }
+        }
+    )
+
+    // map over the results, and deserialize documents array of object property 
+    return results.documents.map((result) => deserialize(result.id, result.value as any))
 };
