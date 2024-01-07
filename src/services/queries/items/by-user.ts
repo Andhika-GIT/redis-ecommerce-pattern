f376323daa4999e8b4049915f279d37d063743fe
Andhika-GIT <andhikapr081@gmail.com>
2024-01-07 18:57:55 +0700

    feat : implement search query for items in dashboard

--- src/services/queries/items/by-user.ts
+++ src/services/queries/items/by-user.ts
@@ -10,10 +10,31 @@ interface QueryOpts {
 }
 
 export const itemsByUser = async (userId: string, opts: QueryOpts) => {
-	const query = `@ownerId:${userId}`;
+	// sort by ownerId field in item hashes
+	const query = `@ownerId:{${userId}}`;
+
+	// create sortCriteria if opts.sortBy and opts.direction available
+	const sortCriteria =
+		opts.sortBy && opts.direction
+			? {
+					BY: opts.sortBy,
+					DIRECTION: opts.direction
+			  }
+			: undefined;
+
+	const { total, documents } = await client.ft.search(itemIndexKey(), query, {
+		ON: 'hash',
+		SORTBY: sortCriteria,
+		LIMIT: {
+			from: opts.page * opts.perPage,
+			size: opts.perPage
+		}
+	} as any);
 
 	return {
-		totalPage: 0,
-		items: []
+		totalPage: Math.ceil(total / opts.perPage),
+		items: documents.map((document) => {
+			return deserialize(document.id.replace('item#', ''), document.value as any);
+		})
 	};
 };
