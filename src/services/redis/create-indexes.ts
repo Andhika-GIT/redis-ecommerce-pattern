7b5a61ec780358e9354b2938e2e9c494273e7d2b
Andhika-GIT <andhikapr081@gmail.com>
2024-01-07 17:43:07 +0700

    refactor ; add item hash field that is sortable and not

--- src/services/redis/create-indexes.ts
+++ src/services/redis/create-indexes.ts
@@ -1,35 +1,60 @@
-import { SchemaFieldTypes } from "redis";
-import { itemIndexKey, itemKey } from "$services/keys";
-import { client } from "./client";
+import { SchemaFieldTypes } from 'redis';
+import { itemIndexKey, itemKey } from '$services/keys';
+import { client } from './client';
 
 export const createIndexes = async () => {
-    // take all indexes
-    const indexes = await client.ft._LIST()
+	// take all indexes
+	const indexes = await client.ft._LIST();
 
-    // check if item index key already exist inside index list
-    const exist = indexes.find((index) => index === itemIndexKey())
+	// check if item index key already exist inside index list
+	const exist = indexes.find((index) => index === itemIndexKey());
 
+	if (exist) {
+		return;
+	}
 
-    if (exist) {
-        return
-    }
+	return client.ft.create(
+		// create index
+		itemIndexKey(),
 
-    return client.ft.create(
-        // create index
-        itemIndexKey(),
-
-        // define the field of the item hash and also the type
-        {
-            name: {
-                type: SchemaFieldTypes.TEXT
-            },
-            description: {
-                type: SchemaFieldTypes.TEXT
-            }
-        },
-        {
-            ON: 'HASH',
-            PREFIX: itemKey('')
-        }
-    )
+		// define the field of the item hash and also the type
+		{
+			name: {
+				type: SchemaFieldTypes.TEXT,
+				SORTABLE: true
+			},
+			description: {
+				type: SchemaFieldTypes.TEXT,
+				SORTABLE: false
+			},
+			ownerId: {
+				type: SchemaFieldTypes.TAG,
+				SORTABLE: false
+			},
+			endingAt: {
+				type: SchemaFieldTypes.NUMERIC,
+				SORTABLE: true
+			},
+			bids: {
+				type: SchemaFieldTypes.NUMERIC,
+				SORTABLE: true
+			},
+			views: {
+				type: SchemaFieldTypes.NUMERIC,
+				SORTABLE: true
+			},
+			price: {
+				type: SchemaFieldTypes.NUMERIC,
+				SORTABLE: true
+			},
+			likes: {
+				type: SchemaFieldTypes.NUMERIC,
+				SORTABLE: true
+			}
+		} as any,
+		{
+			ON: 'HASH',
+			PREFIX: itemKey('')
+		}
+	);
 };
